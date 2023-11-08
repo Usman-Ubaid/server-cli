a15b10c9f4062ec38ff098c3460d54614e12d5d5
Welcome ! <usmanubaid9@gmail.com>
2023-11-08 23:15:46 +0100

    feat: add get, remove, find, clean commands

--- src/command.js
+++ src/command.js
@@ -15,5 +15,59 @@ yargs(hideBin(process.argv))
       console.log("New note added: ");
     }
   )
+  .option("tags", {
+    alias: "t",
+    type: "string",
+    description: "tags to add note to the note",
+  })
+  .command(
+    "all",
+    "get all notes",
+    () => {},
+    async () => {
+      console.log("All notes fetched");
+    }
+  )
+  .command(
+    "find <filter>",
+    "get matching notes",
+    (yargs) => {
+      return yargs.positional("filter", {
+        describe:
+          "The search term to filter notes by, will be applied to note.content",
+        type: "string",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "remove <id>",
+    "remove a note by id",
+    (yargs) => {
+      return yargs.positional("id", {
+        type: "number",
+        description: "The id of the note you want to remove",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "web [port]",
+    "launch website to see notes",
+    (yargs) => {
+      return yargs.positional("port", {
+        describe: "port to bind on",
+        default: 5000,
+        type: "number",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "clean",
+    "remove all notes",
+    () => {},
+    async (argv) => {}
+  )
   .demandCommand(1)
   .parse();
