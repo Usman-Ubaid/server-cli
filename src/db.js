c45101946fe64a70c95ddc1add4ce83b4ebd3749
Welcome ! <usmanubaid9@gmail.com>
2023-11-09 00:02:38 +0100

    feat: writing to db.json & adding data to db.json

--- src/db.js
+++ src/db.js
@@ -6,3 +6,15 @@ export const getDB = async () => {
   const db = await fs.readFile(DB_PATH, { encoding: "utf-8" });
   return JSON.parse(db);
 };
+
+export const saveDB = async (db) => {
+  await fs.readFile(DB_PATH, JSON.stringify(db, null, 2));
+  return db;
+};
+
+export const insert = async (data) => {
+  const db = await getDB();
+  db.notes.push(data);
+  await saveDB(db);
+  return data;
+};
